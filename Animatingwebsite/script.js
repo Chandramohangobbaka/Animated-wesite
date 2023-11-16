f53799136e164092c2cbebd66a5edc487240adfc
Chandramohangobbaka <gobbaka2004chandra@gmail.com>
2023-11-16 13:59:55 +0530

    Add some animations

--- Animatingwebsite/script.js
+++ Animatingwebsite/script.js
@@ -2,10 +2,24 @@
 var crsr=document.querySelector("#cursor")
 var blur=document.querySelector("#cursor-blur")
 document.addEventListener("mousemove",function(dets){
-    crsr.style.left=dets.x+"px"
+    crsr.style.left=dets.x+25+"px"
     crsr.style.top=dets.y+"px"
     blur.style.left=dets.x-250+"px"
     blur.style.top=dets.y-250+"px"
+})
+
+var h4all=document.querySelectorAll("#nav h4")
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        crsr.style.scale=3
+        crsr.style.border="1px solid #fff"
+        crsr.style.backgroundColor="transparent"
+    })
+    elem.addEventListener("mouseleave",function(){
+        crsr.style.scale=1
+        crsr.style.border="0px solid #95C11E"
+        crsr.style.backgroundColor="#95C11E"
+    })
 })
 
 gsap.to("#nav",{
@@ -33,3 +47,28 @@ gsap.to("#main",{
         scrub:2
     }
 })
+gsap.from("#about-us img,#about-us-in",{
+    y:90,
+    opacity:0,
+    duration:1,
+    stagger:0.4,
+    scrollTrigger:{
+        trigger:"#about-us",
+        scroller:"body",
+        start:"top 60%",
+        end:"top 55%",
+        scrup:2
+    }
+})  
+gsap.from(".card",{
+    scale:0.8,
+    opacity:0,
+    duration:1,
+    scrollTrigger:{
+        trigger:".card",
+        scroller:"body",
+        start:"top 70%",
+        end:"top 65%",
+        scrup:2
+    }
+})  
